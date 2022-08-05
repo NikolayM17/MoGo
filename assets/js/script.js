674db96d10359e82f61bc38cbe2dd19f9b8505a7
NikolayM17 <17nikolaym@mail.ru>
2022-08-05 10:58:42 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const name = "Riddla";
+let age = 19;
+let isTrue = true;
+
+console.log(typeof age, age);
+
+age = "Девятнадцать";
+
+console.log(typeof age, age);
+
+console.log(typeof name, name);
+
+console.log(typeof isTrue, isTrue);
+
+//  const header = document.querySelector(".header");
+const header = document.getElementById("header");
+const navLinks = document.querySelectorAll(".nav__link");
+
+console.log(typeof header, header);
+console.log(typeof navLinks, navLinks);
